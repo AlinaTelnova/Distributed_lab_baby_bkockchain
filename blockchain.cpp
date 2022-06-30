43ca3ab8b98b5379092fab5edac686e1594e3f37
AlinaTelnova <105862476+AlinaTelnova@users.noreply.github.com>
2022-06-30 13:14:15 +0300

    Update blockchain.cpp

--- blockchain.cpp
+++ blockchain.cpp
@@ -3,75 +3,87 @@
 #include <cstdlib>
 #include <iomanip>
 #include <cstring>
+#include <vector>
 using namespace std;
 int sundaram(int n)
 {
-	int* a = new int[n], i, j, k;
-	memset(a, 0, sizeof(int) * n);
-	for (i = 1; 3 * i + 1 < n; i++)
-	{
-		for (j = 1; (k = i + j + 2 * i * j) < n && j <= i; j++)
-		{
-			a[k] = 1;
-		}
-	}
-	for (i = n - 1; i >= 1; i--)
-	{
-		if (a[i] == 0)
-		{
-			return (2 * i + 1);
-			break;
-		}
-	}
-	delete[] a;
+  int* a = new int[n], i, j, k;
+  memset(a, 0, sizeof(int) * n);
+  for (i = 1; 3 * i + 1 < n; i++)
+  {
+    for (j = 1; (k = i + j + 2 * i * j) < n && j <= i; j++)
+    {
+      a[k] = 1;
+    }
+  }
+  for (i = n - 1; i >= 1; i--)
+  {
+    if (a[i] == 0)
+    {
+      return (2 * i + 1);
+      break;
+    }
+  }
+  delete[] a;
 }
 int gcd(int a, int b)
 {
-	int c;
-	while (b)
-	{
-		c = a % b;
-		a = b;
-		b = c;
-	}
-	return abs(a);
+  int c;
+  while (b)
+  {
+    c = a % b;
+    a = b;
+    b = c;
+  }
+  return abs(a);
 }
 class KeyPair
 {
 private:
-	unsigned long long int privateKey;
+  vector <int> privateKey;
 public:
-	unsigned long long publicKey;
-	void genPairKey()
-	{
-		int p = rand() % 10000000000000000000;
-		int q = rand() % 10000000000000000000;
-		int p_prime = sundaram(p);
-		int q_prime = sundaram(q);
-		unsigned int n = p_prime * q_prime;
-		int e, e_prime = 0;
-		while (e_prime != 1)
-		{
-			e = rand() % 10000000000000000000;
-			e_prime = gcd(e, ((p_prime - 1) * (q_prime - 1)));
-		}
-		unsigned int d = 0, d_prime = 0;
-		while (d_prime != 1)
-		{
-			d += 1;
-			d_prime = (e * d) % ((p_prime - 1) * (q_prime - 1));
-		}
-		publicKey = e;
-		privateKey = d;
-	}
-	void printKeyPair()
-	{
-		cout << publicKey << '\t' << privateKey << endl;
-	}
+  vector <int> publicKey;
+  void genPairKey()
+  {
+    int p = rand() % 10000000000000000000;
+    int q = rand() % 10000000000000000000;
+    int p_prime = sundaram(p);
+    int q_prime = sundaram(q);
+    unsigned int n = p_prime * q_prime;
+    int e, e_prime = 0;
+    while (e_prime != 1)
+    {
+      e = rand() % 10000000000000000000;
+      e_prime = gcd(e, ((p_prime - 1) * (q_prime - 1)));
+    }
+    unsigned int d = 0, d_prime = 0;
+    while (d_prime != 1)
+    {
+      d += 1;
+      d_prime = (e * d) % ((p_prime - 1) * (q_prime - 1));
+    }
+    publicKey.push_back(e);
+    publicKey.push_back(n);
+    privateKey.push_back(d);
+    privateKey.push_back(n);
+  }
+  void printKeyPair()
+  {
+    for (int i = 0; i < publicKey.size(); i++)
+    {
+      cout << publicKey[i] << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < privateKey.size(); i++)
+    {
+      cout << privateKey[i] << " ";
+    }
+    cout << endl;
+  }
 };
 int main()
 {
-	KeyPair keypair;
-	keypair.genPairKey();
-	keypair.printKeyPair();
+  KeyPair keypair;
+  keypair.genPairKey();
+  keypair.printKeyPair();
 }
